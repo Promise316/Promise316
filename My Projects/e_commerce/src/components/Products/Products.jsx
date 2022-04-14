c6de40f4afc744b9972de47eb3fc56ab632407f3
Promise316 <chilufyajohn01@gmail.com>
2022-04-14 22:57:02 +0200

    ecomm

--- My Projects/e_commerce/src/components/Products/Products.jsx
+++ My Projects/e_commerce/src/components/Products/Products.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 
-import Product from './products/products';
+import Product from './product/Product';
+import useStyles from './styles';
 
-const products = [
-    {id: 1, name: 'T-shirt', description: 'T-shirt.',price: '$5'},
-    {id: 2, name: 'Desktop', description: 'Apple.', price: '$100'},
 
-];
+  const Products = ({products, onAddToCart}) => {
+      const classes = useStyles();
 
-const Products = () => {
     return(
-    <main>
-        <Grid containear justify="center" specing={4}>
-            {products.map((product) =>(
+    <main className={classes.content}>
+        <div className={classes.toolbar}/>
+        <Grid container justify="center" spacing={4}>
+            {products.map((product) =>( 
                 <Grid item key= {product.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product product={product}/>
+                    <Product product={product} onAddToCart={onAddToCart}/>
                 </Grid>
             ))}
         </Grid>
     </main>
     );
-}
+};
 
 export default Products;
